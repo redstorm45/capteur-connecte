0504574dbb57652025e4e14a89f5477df5e1079d
redstorm45 <pierre.cadart@gmail.com>
2018-02-20 10:01:25 +0100

    Changement de lib MQTT

--- capteur/capteur.ino
+++ capteur/capteur.ino
@@ -1,7 +1,7 @@
 #include <ESP8266WiFi.h>
 #include <WiFiUdp.h>
 #include <OneWire.h>
-#include <PubSubClient.h>      // https://github.com/Imroy/pubsubclient
+#include <PubSubClient.h>      // https://github.com/knolleary/pubsubclient
 #include <NTPClient.h>         // https://github.com/arduino-libraries/NTPClient
 #include <DallasTemperature.h> // https://github.com/milesburton/Arduino-Temperature-Control-Library
 #include "buffer.h"
@@ -29,11 +29,13 @@ const char* mqttTopic = "topic";
 // Connexion wi-fi
 WiFiClient espClient;
 // Connexion MQTT
-PubSubClient clientMQTT(espClient, urlBrokerMQTT);
+PubSubClient clientMQTT(espClient);
 // Lien UDP
 WiFiUDP ntpUDP;
 // Client NTP pour l'horodatage des données
 NTPClient timeClient(ntpUDP, urlServeurNTP, OFFSET_NTP, DELAI_NTP); 
+unsigned long lastTSUpdate;
+unsigned long currentTS;
 
 // Instance OneWire pour le capteur de température
 OneWire oneWire(PIN_CAPTEUR);
@@ -53,9 +55,10 @@ Buffer buffer;
 
 unsigned long timerMesure;
 unsigned long timerEnvoi;
+bool lastMesureDone;
 
 /* *** Définitions des fonctions *** */
-void callback(const MQTT::Publish& pub);
+void callback(char* topic, byte* payload, unsigned int length);
 void setup();
 void tickWifi();
 String format(Mesure m);
@@ -63,7 +66,17 @@ Mesure* capte();
 void loop();
 
 /* *** Fonctions *** */
-void callback(const MQTT::Publish& pub) {}
+void callback(char* topic, byte* payload, unsigned int length) {
+  if(strcmp(topic, "timestamp")==0){
+    unsigned long newTS = 0;
+    for(int i=0; i<length; i++){
+      newTS = newTS*10 + payload[i] - int('0');
+    }
+    Serial.println("got ts:"+String(newTS));
+    lastTSUpdate = millis();
+    currentTS = newTS;
+  }
+}
 
 void setup() {
   // Initialiser la connexion wifi
@@ -84,6 +97,10 @@ void setup() {
   etatWifi = true;
   // NTP et MQTT déjà initialisés (objets créés)
   timeClient.begin();
+  clientMQTT.setServer(urlBrokerMQTT, 1883);
+  clientMQTT.setCallback(callback);
+  //timeClient.begin();
+  delay(500);
   // Connexion au capteur
   capteurs.begin();
   capteurs.getAddress(adresseCapteur, 0);
@@ -109,9 +126,8 @@ String format(Mesure* m){
 }
 
 Mesure* capte(){
-  capteurs.requestTemperatures();
   Mesure* m=new Mesure;
-  (*m) = {.valeur = capteurs.getTempC(adresseCapteur), .timestamp = timeClient.getEpochTime()};
+  (*m) = {.valeur = capteurs.getTempC(adresseCapteur), .timestamp = (currentTS+(long)(millis()-lastTSUpdate)/1000)};//timeClient.getEpochTime()};
   return m;
 }
 
@@ -123,8 +139,11 @@ void loop() {
   // MQTT
   if (WiFi.status() == WL_CONNECTED) {
     if (!clientMQTT.connected()) {
+      #ifdef DEBUG
+        Serial.println("reconnection au broker mqtt...");
+      #endif
       if(clientMQTT.connect(mqttUsername)){
-        clientMQTT.set_callback(callback);
+        clientMQTT.subscribe("timestamp");
         #ifdef DEBUG
           Serial.println("connecté au broker mqtt");
         #endif
@@ -136,25 +155,41 @@ void loop() {
       #endif
     }
     else{
-      if(millis()-timerEnvoi >= DELAI_ENVOI){
-        Serial.println("envoi possible");
+      clientMQTT.loop();
+      if((millis()-timerEnvoi) >= DELAI_ENVOI){
+        #ifdef DEBUG
+          Serial.println("envoi possible");
+        #endif
         if(buffer.disponible()){
+          #ifdef DEBUG
+            Serial.println("envoi!");
+          #endif
           Mesure* m = buffer.popOldestData();
-          clientMQTT.publish(MQTT::Publish(mqttTopic, format(m)).set_qos(2));
+          String s=format(m);
+          byte* data=(byte*)malloc(s.length()+1);
+          s.getBytes(data, s.length()+1);
+          clientMQTT.publish(mqttTopic, data, s.length()+1);
+          free(data);
           delete m;
         }else{
           //Une fois que toutes les données ont été envoyée, on considere que c'est la fin du paquet.
           timerEnvoi = millis();
         }
       }
-      clientMQTT.loop();
     }
   }
   // Capteur
-  if(millis()-timerMesure >= buffer.isInLightMode()?DELAI_MESURE_ECRASANT:DELAI_MESURE){
+  if((millis()-timerMesure) >= (buffer.isInLightMode()?DELAI_MESURE_ECRASANT:DELAI_MESURE)){
     timerMesure += buffer.isInLightMode()?DELAI_MESURE_ECRASANT:DELAI_MESURE;
+    capteurs.requestTemperatures();
+  }
+  if(((millis()-timerMesure) >= DELAI_MESURE_CONVERSION) && lastMesureDone){
+    lastMesureDone = false;
     Mesure* m=capte();
     buffer.addData(m);
+    #ifdef DEBUG
+      Serial.println("mesure!");
+    #endif
   }
   // Lib du capteur
   yield();
